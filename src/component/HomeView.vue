56706ec3909e0a190fd31fcc22e9d5c7dab7db8c
SilianZ <silian_zheng@outlook.com>
2024-07-26 18:54:08 +0800

    :sparkles: 更新 StatsComponent

--- src/component/HomeView.vue
+++ src/component/HomeView.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
-import Skeleton from 'primevue/skeleton'
-import HitsChart from './charts/HitsChart.vue'
-import { computed, ref, type Ref } from 'vue'
-import { type StatsData, type StatsRes } from '@/api'
-import { fetchStat } from '@/api'
-import { useRequest } from 'vue-request'
 import StatsComponent from './StatsComponent.vue'
 
-const { data } = useRequest((): Promise<StatsRes> => fetchStat(), { pollingInterval: 10000 })
 </script>
 
 <template>
     <h1>Python OpenBMCLAPI 仪表盘</h1>
-    <StatsComponent :stats="data?.startTime"></StatsComponent>
+    <StatsComponent />
 </template>
--- src/component/StatsComponent.vue
+++ src/component/StatsComponent.vue
@@ -2,25 +2,56 @@
 import HitsChart from '@/component/charts/HitsChart.vue'
 import { type StatsRes } from '@/api';
 import { computed, ref } from 'vue';
+import { useRequest } from 'vue-request';
+import { fetchStat } from '@/api';
 
-const props = defineProps<{
-    stats: StatsRes
-}>()
+const { data } = useRequest((): Promise<StatsRes> => fetchStat(), { pollingInterval: 10000 })
 
-console.log(props.stats.startTime)
-
-const stats = computed(() => {
-    return !props.stats ? null : props.stats
-})
-
-const startTime = props.stats.startTime
+const startTime = computed(() => {
+    if (data !== undefined) return data.value.startTime
+    return null
+}).value
 const nowTime = ref(new Date())
 
+console.log(nowTime.value.getTime())
+
 const hours = computed(() => {
-    return startTime !== null ? ((nowTime.value.getTime() - startTime) / (60 * 60 * 1000)).toFixed(2).toString : 'NaN'
+    return startTime !== null ? (((nowTime.value.getTime() - startTime) / (60 * 60 * 1000)).toFixed(2).toString()) : 'NaN'
 })
+
+const currentTime = ref({
+    hour: nowTime.value.getHours(),
+    date: nowTime.value.getDate(),
+    year: nowTime.value.getFullYear()
+})
+
+const formatHours = (i: number) => {
+    if (i < 0) i = 24 + i
+    return `${i >= 10 ? i.toString() : `0${i.toString()}`}:00`
+}
+
 </script>
 
 <template>
-<h2><i>服务器已运行 {{ hours }} 小时。</i></h2>
+<h3><i>服务器已运行 {{ hours }} 小时。</i></h3>
+
+<!-- hours -->
+<HitsChart
+    v-if="data"
+    id="hits"
+    :data="data.stats.hours"
+    :oldData="data.prevStats.hours"
+    :current="currentTime.hour"
+    :max="21"
+    :offset="20"
+    :formatX="formatHours"
+/>
+<!-- <Skeleton v-else height="13rem" width="45rem" /> -->
 </template>
+
+<style scoped>
+#hits {
+    height: 13rem;
+    width: 45rem;
+}
+</style>
